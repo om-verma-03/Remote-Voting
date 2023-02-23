22e7cb891f3363748f322c4ed367f5f828407fa0
ShubhamAgarwal-11 <shubham2002jindal@gmail.com>
2023-02-24 00:15:04 +0530

    setup model view comtroller

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const port = 4400;
 const app = express();
+const sassMiddleware = require('node-sass-middleware');
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(sassMiddleware({
+    src: '/assets/scss',
+    dest: '/assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
 
-app.get('/',function(req,res){
-    res.send('Hello');    
-})
+app.use(express.urlencoded());
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+app.use('/', require('./routers'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err) { console.log(`Error in creating a server`); return; }
 
